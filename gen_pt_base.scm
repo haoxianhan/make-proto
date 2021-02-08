d3821395ba96e4c7fa25458edb938c0d76060838
haoxian <haoxianhan@gmail.com>
2021-02-09 00:16:52 +0800

    add comment

--- gen_pt_base.scm
+++ gen_pt_base.scm
@@ -2,7 +2,8 @@
 
 (provide valid-pt-num?
          valid-pt-type?
-         valid-pt-marco?)
+         valid-pt-marco?
+		 valid-pt-comment?)
 
 
 (define (valid-pt-num? pt)
@@ -20,3 +21,8 @@
   (and (hash-has-key? pt "pt-marco")
        (let ((pt-marco (hash-ref pt "pt-marco")))
          (string? pt-marco))))
+
+(define (valid-pt-comment? pt)
+  (and (hash-has-key? pt "pt-comment")
+       (let ((pt-comment (hash-ref pt "pt-comment")))
+         (string? pt-comment))))
