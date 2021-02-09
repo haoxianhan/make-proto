256a45da4fea23185e970df6be2618cf9b745f7c
haoxian <haoxianhan@gmail.com>
2021-02-09 11:16:48 +0800

    rename

---
+++ marco2num.rkt
@@ -0,0 +1,50 @@
+#lang racket
+
+(require "utils.rkt")
+
+(provide make-pt-marco)
+
+(define (make-pt-marco)
+  (define marco-list '())
+
+  (define (print-marco-list)
+    (map (lambda(x) (display x)
+                    (newline))
+         (reverse marco-list)))
+
+  (define (write-file)
+    (call-with-output-file "./proto.hrl"
+                           (lambda (out)
+                             (map (lambda(e) (displayln e out))
+                                  (reverse marco-list)))
+                           #:exists 'replace))
+
+  (define (add-marco new-marco)
+    (set! marco-list (cons new-marco marco-list)))
+
+  (define (can-add-marco? pt)
+    (and (valid-pt-num? pt)
+         (valid-pt-marco? pt)
+         (valid-pt-type? pt)
+         (valid-pt-comment? pt)))
+
+  (define (formatter pt)
+    (let* ((pt-num (hash-ref pt "pt-num"))
+           (pt-comment (hash-ref! pt "pt-comment" ""))
+           (pt-type (hash-ref pt "pt-type"))
+           (pt-marco (hash-ref pt "pt-marco"))
+           (marco-name (string-upcase (string-append pt-type "_" pt-marco))))
+           (string-append "-define(?" marco-name ", " (number->string pt-num) "). %% " pt-comment)))
+
+  (define (try-add-pt-marco pt)
+    (if (can-add-marco? pt)
+      (add-marco (formatter pt))
+      (error "pt not valid" pt)))
+
+  (define (disp opt)
+    (cond ((eq? opt 'add) try-add-pt-marco)
+          ((eq? opt 'print) (write-file))
+          ((eq? opt 'write-file) (write-file))
+          (else (error "make-pt-marco wrong opt"))))
+
+  disp)
---
+++ utils.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(provide
+  valid-pt-num?
+  valid-pt-type?
+  valid-pt-marco?
+  valid-pt-alias?
+  valid-pt-comment?)
+
+
+(define (valid-pt-num? pt)
+  (and (hash-has-key? pt "pt-num")
+       (let ((pt-num (hash-ref pt "pt-num")))
+         (number? pt-num))))
+
+(define (valid-pt-type? pt)
+  (and (hash-has-key? pt "pt-type")
+       (let ((pt-type (hash-ref pt "pt-type")))
+         (or (equal? pt-type "c")
+             (equal? pt-type "s")))))
+
+(define (valid-pt-marco? pt)
+  (and (hash-has-key? pt "pt-marco")
+       (let ((pt-marco (hash-ref pt "pt-marco")))
+         (string? pt-marco))))
+
+(define (valid-pt-comment? pt)
+  (and (hash-has-key? pt "pt-comment")
+       (let ((pt-comment (hash-ref pt "pt-comment")))
+         (string? pt-comment))))
+
+(define (valid-pt-alias? pt)
+  (and (hash-has-key? pt "pt-alias")
+       (let ((pt-alias (hash-ref pt "pt-alias")))
+         (string? pt-alias))))
+
